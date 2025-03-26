ef40df4871e529fd76c1d0cbab0e23e3057bfb04
Maaz Amir <amirmaaz16@gmail.com>
2025-03-27 00:10:14 +0530

    Search Form and reset component created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
+import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
+  const query = (await searchParams).query;
   return (
     <>
-    <section>
-      
-    </section>
-     <h1 className="heading">HOME</h1>
+      <section className="pink_container">
+
+        <h1 className="heading">Pitch Your Startup, <br /> Connect with Enterpreneurs</h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query}/>
+      </section>
     </>
   );
 }
